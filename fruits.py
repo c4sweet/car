8a826e3544367cf404e427d75635bb3e9ba87773
c4sweet <porpakon1991@gmail.com>
2021-01-28 13:27:23 +0700

    updata

---
+++ fruits.py
@@ -0,0 +1,11 @@
+import xlsxwriter
+import pandas as pd
+
+dataframe = pd.DataFrame({'Data' : [20,30,45,12,33,9]})
+
+writer = pd.ExcelWriter('simple_data.xlsx', engine='xlswriter')
+
+dataframe.to_excel(writer,sheet_name='หน้าที่1')
+
+writer.save()
+
